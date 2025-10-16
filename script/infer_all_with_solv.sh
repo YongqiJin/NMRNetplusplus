a4e02575be5e20a4295724d19a3b3e5e3b897f3a
Yecheng Wang <wyecheng48@gmail.com>
2025-10-16 23:15:26 +0800

    Update infer_all_with_solv.sh

--- script/infer_all_with_solv.sh
+++ script/infer_all_with_solv.sh
@@ -1,14 +1,16 @@
-data_path="./data/nmrshiftdb2_2018/All" # replace to your data path
+ata_path="./data/nmrshiftdb2_2018/All" # replace to your data path
 
+#save_dir="./output/unlabel/nmrshiftdb2_2018/5cv/pretrain_H_checkpoint_best_unimol_large_solv_atom_regloss_mae_lr_0.0001_bs1_4_bs2_16_wu_0.03_ep_20_wgt_1_T__ratio_4_16"  # replace to your finetune ckpt path
 save_dir="$1"  # replace to your finetune ckpt path
-#save_dir="./output/unlabel/nmrshiftdb2_2018/5cv/pretrain_H_checkpoint_best_unimol_large_solv_atom_regloss_mae_lr_0.0001_bs1_4_bs2_16_wu_0.03_ep_20_wgt_1_T__ratio_4_16_20250908_055158"  # replace to your finetune ckpt path
 element="$2"   # replace to your labeled atom
+arch="$3"      # replace to your arch, e.g., unimol_large_solv
+solvent_flags="$4" 
 unlabeled_data_path="./data/NMRexp_v0905/${element}" # replace to your unlabeled data path
 
 
 mkdir -p ${save_dir}
 
-nfolds=1
+nfolds=5
 for fold in $(seq 0 $(($nfolds - 1)))
     do
     echo "Start infering..."
@@ -16,20 +18,20 @@ for fold in $(seq 0 $(($nfolds - 1)))
     python ./uninmr/infer.py --user-dir  ./uninmr   ${data_path}   --valid-subset valid \
         --results-path ${save_dir}/cv_seed_${cv_seed}_fold_${fold}  --saved-dir $save_dir \
         --num-workers 8 --ddp-backend=c10d --batch-size 64 \
-        --task uninmr_solv --loss 'atom_regloss_mae' --arch 'unimol_large_solv' \
-        --dict-name "dict.txt" --solvent-embed-dim 16\
+        --task uninmr_solv --loss 'atom_regloss_mae' --arch "${arch}" \
+        --dict-name "dict.txt" \
+        ${solvent_flags} \
         --path ${save_dir}/cv_seed_${cv_seed}_fold_${fold}/checkpoint_last.pt \
         --fp16 --fp16-init-scale 4 --fp16-scale-window 256 \
         --log-interval 50 --log-format simple --required-batch-size-multiple 1 \
-        --selected-atom ${element}   --gaussian-kernel   --atom-descriptor 0 --split-mode infer \
-        --solvent-embed-dim 16  --solvent-embed-after-backbone True --solvent-embed-before-backbone False 
+        --selected-atom ${element}   --gaussian-kernel   --atom-descriptor 0 --split-mode infer 
     done 2>&1 | tee ${save_dir}/infer.log
 
 python ./uninmr/utils/get_result.py --path ${save_dir} --file_end "*valid.out.pkl" --mode cv --dict "./data/nmrshiftdb2_2018/All/dict.txt" 2>&1 | tee ${save_dir}/result.log
 python ./uninmr/utils/get_result_unlabel.py --path ${save_dir} --file_end "*valid.out.pkl" --mode cv --dict "./data/nmrshiftdb2_2018/All/dict.txt" 2>&1 | tee ${save_dir}/result_unlabel_1.log
 
 
-nfolds=1
+nfolds=5
 for fold in $(seq 0 $(($nfolds - 1)))
     do
     echo "Start infering..."
@@ -37,14 +39,14 @@ for fold in $(seq 0 $(($nfolds - 1)))
     python ./uninmr/infer.py --user-dir  ./uninmr   ${unlabeled_data_path}   --valid-subset valid  --subset_name valid_unlabel \
         --results-path ${save_dir}/cv_seed_${cv_seed}_fold_${fold}  --saved-dir $save_dir \
         --num-workers 8 --ddp-backend=c10d --batch-size 64 \
-        --task uninmr_solv --loss 'atom_regloss_mae' --arch 'unimol_large_solv' \
-        --dict-name "dict.txt" --solvent-embed-dim 16\
+        --task uninmr_solv --loss 'atom_regloss_mae' --arch "${arch}" \
+        --dict-name "dict.txt"\
+        ${solvent_flags} \
         --path ${save_dir}/cv_seed_${cv_seed}_fold_${fold}/checkpoint_last.pt \
         --fp16 --fp16-init-scale 4 --fp16-scale-window 256 \
         --log-interval 50 --log-format simple --required-batch-size-multiple 1 \
-        --selected-atom ${element}   --gaussian-kernel   --atom-descriptor 0 --split-mode infer \
-        --solvent-embed-dim 16  --solvent-embed-after-backbone True --solvent-embed-before-backbone False
+        --selected-atom ${element}   --gaussian-kernel   --atom-descriptor 0 --split-mode infer 
+
     done 2>&1 | tee ${save_dir}/infer_unlabel.log
 
-python ./uninmr/utils/get_result_unlabel.py --path ${save_dir} --file_end "*valid_unlabel.out.pkl" --mode cv --dict "./data/nmrshiftdb2_2018/All/dict.txt" 2>&1 | tee ${save_dir}/result_unlabel_2.log
-
+python ./uninmr/utils/get_result_unlabel.py --path ${save_dir} --file_end "*valid_unlabel.out.pkl" --mode cv --dict "./data/nmrshiftdb2_2018/All/dict.txt" 2>&1 | tee ${save_dir}/result_unlabel_2.log 
